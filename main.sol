5fb90eb3a7839b07ed42aa1646816fbc8ab519c1
Dobrin Donev <d.d.donev@abv.bg>
2021-02-08 23:43:38 +0200

    Add function documentation

--- main.sol
+++ main.sol
@@ -27,8 +27,16 @@ contract CopyPasteDoneWrong {
 
     constructor() {
         owner = msg.sender;
+        emit OwnerSet(address(0), owner);
     }
 
+    /**
+     * @dev Add a picture to the mapping.
+     * @param pictureFile the hash of the file.
+     * @param picturePrice the price of the picture.
+     * @param forSale is it for sale.
+     * @param id picture's id.
+     */
     function addPicture(
         string memory pictureFile,
         uint256 picturePrice,
@@ -36,35 +44,70 @@ contract CopyPasteDoneWrong {
         string memory id
     ) public {}
 
+    /**
+     * @dev Buyer gets ownership.
+     * @param pictureId the id of the demanded picture.
+     */
     function buyPicture(string memory pictureId) external payable {}
 
+    /**
+     * @dev Gets owned pictures.
+     */
     function getOwnedPictures() public returns (string[] memory) {}
 
+    /**
+     * @dev Gets bought pictures.
+     */
     function getBoughtPictures() public returns (string[] memory) {}
 
+    /**
+     * @dev Gets last N pictures.
+     */
     function getLastNPictures(uint256 count)
         public
         returns (Picture[] memory)
     {}
 
+    /**
+     * @dev Gets first N pictures.
+     */
     function getFirstNPicturesu(int256 count)
         public
         returns (Picture[] memory)
     {}
 
+    /**
+     * @dev Sets pictures price. Owner only.
+     * @param pictureId The id of the picture.
+     * @param newPrice The new price;
+     */
     function setPrice(string memory pictureId, uint256 newPrice)
         external
         isOwner
     {}
 
-    function setForSale(string memory pictureId, bool status)
+    /**
+     * @dev Change selling status.
+     * @param pictureId The id of the picture.
+     * @param newStatus The new status;
+     */
+    function setForSale(string memory pictureId, bool newStatus)
         external
         isOwner
     {}
 
+    /**
+     * @dev Transfers the ownership to the buyer.
+     * @param newOwnerID The address of the buyer.
+     * @param pictureId The id of the picture.
+     */
     function transferOwnership(address newOwnerID, string memory pictureId)
         public
     {}
 
+    /**
+     * @dev Removes a picture from the database.
+     * @param pictureId The id of the picture to be deleted.
+     */
     function deletePicture(string memory pictureId) public isOwner {}
 }
